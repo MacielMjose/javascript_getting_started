18322424530504963278debcd73df6467f0b5762
josé maciel <jlmm92.maciel@gmail.com>
2021-01-31 22:32:06 -0300

    Added new code in home.js

--- 02/demos/after/js-demo/home.js
+++ 02/demos/after/js-demo/home.js
@@ -1,8 +1,67 @@
 //changes the main box text
 /*
     multiple lines comments
+
+let price = 99.99,
+    name = 'Hiking Boots',
+    discounted = false;
+let times99 = 99;
+let msg;
+
+var pricce = 20;
+
+showMessage(pricce);
+console.log(price, name, discounted);
+console.log(pricce);
+
+let hello = "Hello \"World\"";
+let nome = 'Andrea';
+let interpolation = `Hello ${nome}`;
+
+//numbers
+
+let priccce = 1.1 + 1.3,
+    taxRate = 0.07,
+    amount = 20 - (-2);
+
+showMessage(interpolation);
 */
-alert('Hello World!');
-var retorno = showMessage("Custom Message");
-showMessage("Custom Message 2");
-console.log(retorno);
+
+//strings
+
+
+//objects
+
+let person = {
+    firstName: 'José',
+    lastName: 'Maciel'
+};
+
+let message = 'Hello',
+    number =  Number.parseFloat('ADF123.12'),
+    amount = number;
+
+//message = message.toLocaleLowerCase() + ' World';
+message = message.toUpperCase();
+message = message.length;
+console.log(message + ":" + typeof person);
+
+//if else statements
+price = 15;
+
+if (price <= 15) {
+    showMessage('discounted');
+}
+
+if(0){
+    showMessage('is truty');
+}
+else{
+    showMessage("it's falsy");
+}
+if( +(1.1 + 1.3).toFixed(2) !==  2.4){
+    showMessage('true');
+}
+else showMessage();
+
+
---
+++ 02/demos/variables.js
@@ -0,0 +1,5 @@
+let total = 149.90;
+let product = 'Hiking Boots';
+let produts = ['product1','product2','product3'];
+let discouted = true;
+
