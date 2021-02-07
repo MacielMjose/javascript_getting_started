be8546e4aec911065b7099c3379314ea55ee6b17
josé maciel <jlmm92.maciel@gmail.com>
2021-02-07 16:42:46 -0300

    lessons about objects

--- 02/demos/after/js-demo/home.js
+++ 02/demos/after/js-demo/home.js
@@ -2,7 +2,38 @@
 /*
     multiple lines comments
 */
-alert('Hello World!');
-var retorno = showMessage("Custom Message");
-showMessage("Custom Message 2");
-console.log(retorno);
+let price = 20;
+
+let message = price > 10 ? 'expensive':'cheap';
+console.log(message);
+
+let myFunction = function loggingFunction(){
+   console.log('here is a message');
+}
+myFunction();
+
+let logInConsole = function(message,antoherMessage){
+    console.log(message,antoherMessage);
+}
+logInConsole('logging in console','another message');
+
+let functReturns = function(value1){
+    return value1 + 10;
+}
+
+logInConsole('returned value:',functReturns(10));
+
+function getSecretkey(){
+
+    let randKey = function(){
+        return Math.random();
+    }
+
+    console.log('generated key:',randKey());
+}
+getSecretkey();
+
+function changePercentOff(percentage){
+    document.getElementById('percent-off').textContent = percentage +'% OFF';
+}
+changePercentOff(50);
---
+++ 02/demos/after/js-demo/person.js
@@ -0,0 +1,29 @@
+let mySymbol = Symbol(); //hides information of an object if declared inside an object
+
+let person = {
+    name:"john",
+    age:32,
+    partTime:false,
+    [mySymbol]: 'secretInformation',
+    showInfo: function(realAge){
+        showMessage(this.name + ' is ' + realAge);
+    }
+};
+
+
+person.age = 47; //person['age'] = 67;
+person.name = "marcelo";
+person.showInfo(56);
+
+
+
+let pessoa = {
+    idade: 32,
+    nome: 'joão da silva'
+};
+
+function incrementarIdade(pessoa){
+    pessoa.idade++;
+}
+incrementarIdade(pessoa);
+showMessage(pessoa.idade);
